9ef5e33adafb89df3583524163c9d42a4e5ef987
Василь Онуфрійчук <83518525+8a7l@users.noreply.github.com>
2021-08-02 06:16:54 +0000

    Create x1y1.php

---
+++ дані/p_script/x1y1.php
@@ -0,0 +1,9 @@
+<?php
+include_once("phaccess.php");
+echo '
+<form method="POST">
+	<input type="hidden" name="ox" value="1">
+	<input type="hidden" name="oy" value="-1">
+	<input type="submit" value="x:1 y:-1">
+</form>';
+?>
